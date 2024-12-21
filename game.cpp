979ed70726464bc3ff504d9bd940a86ecc7d72ff
HamzaAmeen99 <hamzarajpoot2183@gmail.com>
2024-12-21 10:27:42 +0500

    Third Commit

--- game.cpp
+++ game.cpp
@@ -10,6 +10,19 @@ Game::Game()
 	nextBlock = GetRandomBlock();
 	gameOver = false;
 	score = 0;
+	InitAudioDevice();
+	music = LoadMusicStream("sound/game-music.mp3");
+	PlayMusicStream(music);
+	rotateSound = LoadSound("sound/rotate.mp3");
+	clearSound = LoadSound("sound/clear.mp3");
+}
+
+Game::~Game()
+{
+	UnloadSound(rotateSound);
+	UnloadSound(clearSound);
+	UnloadMusicStream(music);
+	CloseAudioDevice();
 }
 
 Block Game::GetRandomBlock()
@@ -135,6 +148,10 @@ void Game::RotateBlock()
 		{
 			currentBlock.UndoRotation();
 		}
+		else
+		{
+			PlaySound(rotateSound);
+		}
 	}
 }
 
@@ -154,7 +171,11 @@ void Game::LockBlock()
 
 	nextBlock = GetRandomBlock();
 	int rowsCleared = grid.ClearFullRows();
-	UpdateScore(rowsCleared, 0);
+	if (rowsCleared > 0)
+	{
+		PlaySound(clearSound);
+		UpdateScore(rowsCleared, 0);
+	}
 }
 
 bool Game::BlockFits()
